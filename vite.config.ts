f5b0f34e615b1ec76bbc1fd6f929c0a933f7808a
Michael Roberts <michael@observerly.com>
2021-12-23 12:37:53 +0000

    Added a library build using vitejs for deploying aestrium as a re-usable Vue 3 component library.

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,6 @@ import WindiCSS from 'vite-plugin-windicss'
 
 import typescript from '@rollup/plugin-typescript'
 
-import { resolve } from 'path'
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -18,29 +16,6 @@ export default defineConfig({
         tsconfig: './tsconfig.json'
     }),
     vue()
-  ],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, '/src')
-    }
-  },
-  build: {
-    lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
-      name: 'aestrium-vue',
-      fileName: format => `aestrium-vue.${format}.js`
-    },
-    rollupOptions: {
-      external: ['vue', '@observerly/celestia'],
-      output: {
-        sourcemap: false,
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          vue: 'Vue'
-        }
-      }
-    }
-  }
+  ]
 })
 
---
+++ vite.lib.config.ts
@@ -0,0 +1,45 @@
+import { defineConfig } from 'vite'
+
+import vue from '@vitejs/plugin-vue'
+import WindiCSS from 'vite-plugin-windicss'
+
+import typescript from '@rollup/plugin-typescript'
+
+import { resolve } from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    // https://github.com/antfu/vite-plugin-windicss
+    WindiCSS({
+      safelist: '',
+    }),
+    typescript({
+        tsconfig: './tsconfig.json'
+    }),
+    vue()
+  ],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, '/src')
+    }
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'aestrium-vue',
+      fileName: format => `aestrium-vue.${format}.js`
+    },
+    rollupOptions: {
+      external: ['vue', '@observerly/celestia'],
+      output: {
+        sourcemap: false,
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  }
+})
