9f619bc9477ed9204275f8cb67a067fc88517c23
Michael J. Roberts <michael@observerly.com>
2022-06-26 16:01:01 +0100

    feat: Added useEquatorialCoordinate() composable.
    feat: Added useEquatorialCoordinate() composable. Includes associated test suite for module export definition and expected output.

---
+++ src/composables/useEquatorialCoordinate/index.md
@@ -0,0 +1,63 @@
+---
+category: Sensors
+---
+
+# useEquatorialCoordinate
+
+Reactive equatorial coordinate
+
+## Usage
+
+```ts
+import { ref } from 'vue'
+
+import { useEquatorialCoordinate } from '@observerly/aestrium'
+
+// Define a star at a known Equatorial Coordinate (e.g., Betelgeuse):
+const betelgeuse = {
+  ra: 88.7929583,
+  dec: 7.4070639
+}
+
+// Provide the observer, which is a longitude and latitude value, 
+// for example purposes this has been chosen as Manua Kea, Hawaii, US
+const longitude = computed(() => {
+  return -155.468094
+})
+
+const latitude = computed(() => {
+  return 19.820611
+})
+
+// Obtain your canvas dimensions:
+const dimensions = computed(() => {
+  return {
+    x: 2660,
+    y: 980
+  }
+})
+
+// Obtain the screen resolution:
+const resolution = computed(() => {
+  return 2
+})
+
+// For testing we need to specify a date because most calculations are
+// differential w.r.t a time component. We set it to the author's birthday:
+const datetime = ref(new Date('2021-05-14T00:00:00.000+00:00'))
+
+const { ra, dec, alt, az } = useEquatorialCoordinate({
+  longitude: longitude,
+  latitude: latitude,
+  azOffset: ref(0),
+  altOffset: ref(0),
+  dimensions,
+  resolution,
+  datetime: datetime
+})
+
+ra.value = betelgeuse.ra
+dec.value = betelgeuse.dec
+```
+
+See the [Type Declarations](#type-declarations) for more options.
---
+++ src/composables/useEquatorialCoordinate/index.test.ts
@@ -0,0 +1,81 @@
+/**
+ * @jest-environment jsdom
+ */
+import {
+  describe,
+  expect,
+  it
+} from 'vitest'
+
+import {
+  computed,
+  defineComponent,
+  nextTick,
+  ref
+} from 'vue'
+
+import {
+  useEquatorialCoordinate
+} from '../'
+
+describe('useEquatorialCoordinate', () => {
+  it('Should Be Defined', () => {
+    expect(useEquatorialCoordinate).toBeDefined()
+  })
+
+  it('Betelgeuse Should Be Where We Expect It To Be', () => {
+    defineComponent({
+      async setup() {
+        // Define a star at a known Equatorial Coordinate:
+        const betelgeuse = {
+          ra: 88.7929583,
+          dec: 7.4070639
+        }
+
+        //  For testing, we will fix the longitude and latitude to be Manua Kea, Hawaii, US
+        const longitude = computed(() => {
+          return -155.468094
+        })
+
+        const latitude = computed(() => {
+          return 19.820611
+        })
+
+        // Obtain your canvas dimensions:
+        const dimensions = computed(() => {
+          return {
+            x: 2660,
+            y: 980
+          }
+        })
+
+        // Obtain the screen resolution:
+        const resolution = computed(() => {
+          return 2
+        })
+
+        // For testing we need to specify a date because most calculations are
+        // differential w.r.t a time component. We set it to the author's birthday:
+        const datetime = ref(new Date('2021-05-14T00:00:00.000+00:00'))
+
+        const { ra, dec, alt, az } = useEquatorialCoordinate({
+          longitude: longitude,
+          latitude: latitude,
+          azOffset: ref(0),
+          altOffset: ref(0),
+          dimensions,
+          resolution,
+          datetime: datetime
+        })
+
+        ra.value = betelgeuse.ra
+        dec.value = betelgeuse.dec
+
+        await nextTick()
+
+        expect(alt.value).toBeCloseTo(72.78539444063765, 1)
+        expect(az.value).toBeCloseTo(134.44877920325155, 1)
+      }
+    })
+  })
+})
