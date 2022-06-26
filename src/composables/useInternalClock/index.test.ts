ecf6caf804716f78d2d902c14244f1be577d88ae
Michael J. Roberts <michael@observerly.com>
2022-06-26 15:36:18 +0100

    fix(test): Updated test runner suite for useIntervalClock() => deprecated useSetup().

--- src/composables/useInternalClock/index.test.ts
+++ src/composables/useInternalClock/index.test.ts
@@ -1,55 +1,60 @@
 /**
  * @jest-environment jsdom
  */
-import {
+ import {
   describe,
   expect,
   it
 } from 'vitest'
 
 import {
-  useInternalClock
-} from './'
+  defineComponent
+} from 'vue'
 
 import {
-  useSetup
-} from '../../utils'
-
+  useInternalClock
+} from '../'
+ 
 describe('useInternalClock', () => {
   it('Should Be Defined', () => {
     expect(useInternalClock).toBeDefined()
   })
 
   it('Should Display The Current Datetime And Setup', () => {
-    useSetup(async () => {
-      // Setup the internal clock , returning the latest datetime:
-      const {
-        datetime,
-        currentDatetime,
-        setupInternalClock
-      } = useInternalClock({
-      })
+    // eslint-disable-next-line vue/one-component-per-file
+    defineComponent({
+      setup() {
+        // Setup the internal clock , returning the latest datetime:
+        const {
+          datetime,
+          currentDatetime,
+          setupInternalClock
+        } = useInternalClock({
+        })
 
-      expect(currentDatetime.value).toBe(datetime)
+        expect(currentDatetime.value).toBe(datetime)
 
-      setupInternalClock()
+        setupInternalClock()
 
-      expect(currentDatetime.value).not.toBe(datetime)
+        expect(currentDatetime.value).not.toBe(datetime)
+      }
     })
   })
 
   it('Should Display The Current Datetime', () => {
-    useSetup(async () => {
-      // Setup the internal clock , returning the latest datetime:
-      const {
-        datetime,
-        currentDatetime
-      } = useInternalClock({
-        datetime: new Date('2021-05-14T00:00:00.000+00:00'),
-        isLive: false
-      })
-
-      expect(currentDatetime.value).toBe(datetime)
+    // eslint-disable-next-line vue/one-component-per-file
+    defineComponent({
+      setup() {
+        const {
+          datetime,
+          currentDatetime
+        } = useInternalClock({
+          datetime: new Date('2021-05-14T00:00:00.000+00:00'),
+          isLive: false
+        })
+  
+        expect(currentDatetime.value).toBe(datetime)
+      }
     })
   })
 })
