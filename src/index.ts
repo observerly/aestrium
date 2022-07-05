81173ac8d30e19d9a48932c2d26100de65c4d456
Michael J. Roberts <michael@observerly.com>
2022-07-05 18:00:51 +0100

    fix: Added implicit module exports to src/index.ts.
    fix: Added implicit module exports to src/index.ts.

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,76 @@
 // Export all default components:
-export * from './components'
+export {
+  ObserverlyLogo,
+  SkyViewer
+} from './components'
 
 // Export all default composables:
-export * from './composables'
+export type {
+  UseCanvasProps,
+  UseCanvasReturn,
+  UseCardinalsOptions,
+  UseCardinalsProps,
+  UseCardinalsReturn,
+  UseConstellationOptions,
+  UseConstellationsProps,
+  UseConstellationsReturn,
+  UseDimensionsProps,
+  UseDimensionsReturn,
+  UseEclipticOptions,
+  UseEclipticProps,
+  UseEclipticReturn,
+  UseEquatorialCoordinateOptions,
+  UseEquatorialCoordinateProps,
+  UseEquatorialCoordinateReturn,
+  UseInteractionsOptions,
+  UseInteractionsProps,
+  UseInteractionsReturn,
+  UseInternalClockOptions,
+  UseInternalClockProps,
+  UseInternalClockReturn,
+  UseMoonOptions,
+  UseMoonProps,
+  UseMoonReturn,
+  UseObserverOptions,
+  UseObserverProps,
+  UseObserverReturn,
+  UseStarsOptions,
+  UseStarsProps,
+  UseStarsReturn,
+  UseSunOptions,
+  UseSunProps,
+  UseSunReturn
+} from './composables'
+
+export {
+  fetchMajorStars,
+  fetchMinorStars,
+  useAdjustedScreen,
+  useCanvas,
+  useCardinals,
+  useConstellations,
+  useDimensions,
+  useEcliptic,
+  useEquatorialCoordinate,
+  useInteractions,
+  useInternalClock,
+  useMoon,
+  useObserver,
+  useStars,
+  useStarsMagnitude,
+  useSun
+} from './composables'
 
 // Export all default types:
-export * from './types'
+export type {
+  SkyViewerOptions,
+  SkyViewerPosition
+} from './types'
 
 // Export all default utils:
-export * from './utils'
+export {
+  drawBody,
+  drawClosedPath,
+  drawLine,
+  intersectDistance
+} from './utils'
